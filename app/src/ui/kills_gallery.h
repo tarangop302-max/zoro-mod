94c450838009550220efa7f6b550fc8ace1e653d
tarangop302-max <tarangop302@gmail.com>
2026-09-17 14:47:20 +0530

    Create kills_gallery.h

---
+++ app/src/ui/kills_gallery.h
@@ -0,0 +1,10 @@
+#ifndef KILLS_GALLERY_H
+#define KILLS_GALLERY_H
+
+#include <thermite.h>
+
+void ui_kills_gallery_init(tenv *env);
+void ui_kills_gallery(tenv *env);
+void ui_kills_gallery_destroy(tenv *env);
+
+#endif

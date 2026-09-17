6458d8364fd1f4ebcc2c6b49e10b941548d50b41
tarangop302-max <tarangop302@gmail.com>
2026-09-17 14:53:32 +0530

    Update android_path.c

--- app/src/android_path.c
+++ app/src/android_path.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 static char g_files_dir[512] = "";
+static char g_pictures_dir[512] = "";
 
 void android_set_files_dir(const char* path) {
     if (path) {
@@ -14,6 +15,17 @@ void android_set_files_dir(const char* path) {
 
 const char* android_get_files_dir(void) {
     return g_files_dir;
+}
+
+void android_set_pictures_dir(const char* path) {
+    if (path) {
+        strncpy(g_pictures_dir, path, sizeof(g_pictures_dir) - 1);
+        g_pictures_dir[sizeof(g_pictures_dir) - 1] = '\0';
+    }
+}
+
+const char* android_get_pictures_dir(void) {
+    return g_pictures_dir;
 }
 
 #endif

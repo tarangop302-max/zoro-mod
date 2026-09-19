5663eb9c4be636f13d8e464618930a32042684fe
tarangop302-max <tarangop302@gmail.com>
2026-09-19 08:47:05 +0530

    Create kill_review.h

---
+++ app/src/ui/kill_review.h
@@ -0,0 +1,10 @@
+#ifndef KILL_REVIEW_H
+#define KILL_REVIEW_H
+
+#include <thermite.h>
+
+void ui_kill_review_init(tenv *env);
+void ui_kill_review(tenv *env);
+void ui_kill_review_destroy(tenv *env);
+
+#endif

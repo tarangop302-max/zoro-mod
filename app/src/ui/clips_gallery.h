f19d99f3118b712c768f583edfa1c831287425d8
tarangop302-max <tarangop302@gmail.com>
2026-09-20 22:36:15 +0530

    Create clips_gallery.h

---
+++ app/src/ui/clips_gallery.h
@@ -0,0 +1,10 @@
+#ifndef CLIPS_GALLERY_H
+#define CLIPS_GALLERY_H
+
+#include <thermite.h>
+
+void ui_clips_gallery_init(tenv *env);
+void ui_clips_gallery(tenv *env);
+void ui_clips_gallery_destroy(tenv *env);
+
+#endif
